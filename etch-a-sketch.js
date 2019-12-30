c79e46f29d39294056e0cb173e48dbcf20405cfa
allanandresch <allanandresch@gmail.com>
2019-12-30 05:45:27 -0600

    Ultimos Detalles Del Juego

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 50;
 
 
 // Configurar nuestro canvas para dibujo
@@ -16,8 +17,9 @@ let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
+let hue = 0;
 ctx.beginPath(); // Empieza el punto para dibujar
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
@@ -26,9 +28,35 @@ ctx.stroke();
 
 // Escribir una funcion de dibujar, esto lleva destructuring
 function draw({ key }) {
-    console.log(key);
-}
 
+    //Linda arcoiris
+    hue += 10;
+    ctx.strokeStyle = `hsl(${Math.random() * 360},100%,50%)`;
+    console.log(key);
+
+    // Empieza el camino
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    // Mover nuestra x y dependiendo de los valores que el usuario hizo.
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT
+            break;
+        default:
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
 
 // Escribir un handler para las flechas.
 function handleKey(e) {
@@ -42,6 +70,9 @@ function handleKey(e) {
 
 // Una funcion para limpiar.
 
+function clearCanvas() {
+    canvas.classList.add('shake');
+};
 
 
 // Escuchar las flechas.
